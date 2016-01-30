840f7afbe9fe52ecef05a3fe7ca59065953cc138
James A. Donnell Jr <bubbajim715@verizon.net>
2016-01-30 14:57:30 -0500

    Preparing program to use jPasteBin

--- src/com/jamesdonnell/autopastebin/Driver.java
+++ src/com/jamesdonnell/autopastebin/Driver.java
@@ -1,5 +1,10 @@
 package com.jamesdonnell.autopastebin;
 
+/** Driver class for Auto-PasteBin application.
+ * @author James A. Donnell Jr. */
 public class Driver {
 
+	public static void main(String[] args) {
+		
+	}
 }
